a3da147b36fbfb934c1ffa31941bf0afd174f6b6
kalpakjian <kalpakjian@kalpakjiandeMacBook-Air.local>
2025-05-29 17:22:41 +0800

    step 6 : install models

--- listings/admin.py
+++ listings/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Listing
+from django.db import models
+from django.forms import NumberInput
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published', 'price', 'list_date', 'realtor'
+    list_display_links = 'id', 'title'
+    search_fields = 'title', 'description', 'address', 'price'
+    list_filter = 'realtor',
+    list_editable = 'price', 'is_published'
+    list_per_page = 25
+    ordering = ['-id']
+    prepopulated_fields = {'title': ('title',)}
+    formfield_overrides = {
+        models.IntegerField: {'widget': NumberInput(attrs={'size':'10'})}}
+    show_facets = admin.ShowFacets.ALWAYS
+
+admin.site.register(Listing, ListingAdmin)
